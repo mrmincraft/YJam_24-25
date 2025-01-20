c16c59b57308e31b78614be107d6cad023944bc9
M2m1n3 <59541770+mrmincraft@users.noreply.github.com>
2025-01-20 11:49:37 +0100

    feature
    tmp texte update with timer

--- Assets/Script/timebehavior.cs
+++ Assets/Script/timebehavior.cs
@@ -1,19 +1,36 @@
 using UnityEngine;
-
+using TMPro;
 
 public class timebehavior : MonoBehaviour
 {
-    [SerializeField] private float m_Time =
-    [SerializeField] private TMP_Text scoreText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float targetTime = 10.0f;
+    [SerializeField] private TMP_Text TimeText;
+    [SerializeField] private bool pause;
+    private float fillpersent = 0 ;
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
     {
-        
+        TimeText.text = targetTime.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!pause)
+        {
+            targetTime -= Time.deltaTime;
+            TimeText.text = targetTime.ToString();
+            if (targetTime <= 0.0f)
+            {
+                timerEnded();
+                
+            }
+        }
+    }
+
+    void timerEnded()
+    {
+        TimeText.text = "0.000";
+        pause = true;
     }
 }
